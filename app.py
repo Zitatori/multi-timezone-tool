525b7df7310be8d159d7192afd211e69f7eec946
Zitatori <zitatori@gmail.com>
2026-05-31 10:02:27 +0200

    add calfornia

--- app.py
+++ app.py
@@ -203,6 +203,7 @@ const countries = [
 { name: "Ecuador", city: "Quito", flag: "🇪🇨", tz: "America/Guayaquil" },
   { name: "New Zealand", city: "Auckland", flag: "🇳🇿", tz: "Pacific/Auckland" },
   { name: "Vietnam", city: "Ho Chi Minh City", flag: "🇻🇳", tz: "Asia/Ho_Chi_Minh" }
+  { name: "USA", city: "California", flag: "🇺🇸", tz: "America/Los_Angeles" }
 ];
 
 const select = document.getElementById("base-country");
